7abe165c510e45a7ce02dd91510dca2bdeb3d2ee
ethanwithe <fcezaromeroy@gmail.com>
2023-12-08 11:13:38 -0500

    se agrego cdigo para ver la lista de deliverys realisados en el html

--- js/delivery.js
+++ js/delivery.js
@@ -1,15 +1,80 @@
 const url_api = "http://localhost:9000/api/deliv";
 
 function listar() {
+    const resultadoDiv = document.getElementById("resultado");
     axios.get(url_api)
     .then(function(response){
-        console.log(response.data);
+        // Crear tabla y elementos de la tabla
+        const tabla = document.createElement("table");
+        const thead = document.createElement("thead");
+        const tbody = document.createElement("tbody");
+
+        // Encabezados de la tabla
+        const encabezados = ["Nombre", "Teléfono", "Correo","dni", "distrito", "tipo_servicio", "tipo_prenda", "recojo","direccion", "comentario" ]; // Agrega más encabezados según tus datos
+
+        // Crear fila de encabezados
+        const encabezadosRow = document.createElement("tr");
+        encabezados.forEach(encabezado => {
+            const th = document.createElement("th");
+            th.textContent = encabezado;
+            encabezadosRow.appendChild(th);
+        });
+        thead.appendChild(encabezadosRow);
+
+        // Datos de la tabla
+        response.data.forEach(dato => {
+            const fila = document.createElement("tr");
+
+            // Crear celdas con los datos
+            const celdaNombre = document.createElement("td");
+            celdaNombre.textContent = dato.Nombre;
+            const celdaTelefono = document.createElement("td");
+            celdaTelefono.textContent = dato.telefono;
+            const celdaCorreo = document.createElement("td");
+            celdaCorreo.textContent = dato.Correo;
+            const celdaDni = document.createElement("td");
+            celdaDni.textContent = dato.dni;
+            const celdaDistrito = document.createElement("td");
+            celdaDistrito.textContent = dato.distrito;
+            const celdaTipo_servicio = document.createElement("td");
+            celdaTipo_servicio.textContent = dato.tipo_servicio;
+            const celdaTipo_prenda = document.createElement("td");
+            celdaTipo_prenda.textContent = dato.tipo_prenda;
+            const celdaRecojo = document.createElement("td");
+            celdaRecojo.textContent = dato.recojo;
+            const celdaDireccion = document.createElement("td");
+            celdaDireccion.textContent = dato.direccion;
+            const celdaComentario = document.createElement("td");
+            celdaComentario.textContent = dato.comentario;
+
+            // aca se agrega las celdas a la fila
+            fila.appendChild(celdaNombre);
+            fila.appendChild(celdaTelefono);
+            fila.appendChild(celdaCorreo);
+            fila.appendChild(celdaDni);
+            fila.appendChild(celdaDistrito);
+            fila.appendChild(celdaTipo_servicio);
+            fila.appendChild(celdaTipo_prenda);
+            fila.appendChild(celdaRecojo);
+            fila.appendChild(celdaDireccion);
+            fila.appendChild(celdaComentario);
+
+            // codigo para agregar las filas a la tabla
+            tbody.appendChild(fila);
+        });
+
+        // se agrega thead y tbody 
+        tabla.appendChild(thead);
+        tabla.appendChild(tbody);
+
+        // codigo para mostrar la tabla nueva y tambien limpia la tabla al mismo tiempo
+        resultadoDiv.innerHTML = ""; 
+        resultadoDiv.appendChild(tabla);
     })
     .catch(function (error) {
         console.error(error);
     });
 }
-
 function guardar() {
     const nombre = document.getElementById("nombre").value;
     const correo = document.getElementById("correo").value;
